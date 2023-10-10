736119674b1a9e7dd8eb75a372eeb790c6b753d1
Kusuma Sushmitha <sk10689@nyu.edu>
2023-10-09 20:33:03 -0400

    Autocomplete for location

---
+++ location/urls.py
@@ -0,0 +1,7 @@
+# appname/urls.py
+from django.urls import path
+from .views import Location_autocomplete
+
+urlpatterns = [
+    path('autocomplete/', Location_autocomplete, name='location-autocomplete'),
+]
--- location/views.py
+++ location/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import JsonResponse
+from .models import Location
+def Location_autocomplete(request):
+    query = request.GET.get('term', '')
+    results = Location.objects.filter(location_name__icontains=query)  # Adjust the filtering based on your model's fields
+    print(results)
+    data = [{'id': location.id, 'text': location.location_name} for location in results]
+    return JsonResponse(data, safe=False)
