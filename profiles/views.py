7f65f2c0c399f26cdfca9e06555dec4066b3cc6e
zackaidja <zackaidja@gmail.com>
2023-11-01 17:25:16 -0400

    fixed flake8 stuff

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
 from .forms import ProfilePictureForm
 from .models import UserProfile
