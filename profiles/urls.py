605b8425f5012783cb006b3ffff0049aceb506b1
zackaidja <zackaidja@gmail.com>
2023-11-28 23:08:36 -0500

    comment out url

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,5 +4,5 @@ from .views import view_profile, edit_profile
 
 urlpatterns = [
     path("profile/<int:userprofile_id>", view_profile, name="view_profile"),
-    path("profile/edit/", edit_profile, name="edit_profile"),
+    # path("profile/edit/", edit_profile, name="edit_profile"),
 ]
