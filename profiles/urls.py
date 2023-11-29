b28ba86820e1590dd23b2b8884a326a151bcafe3
zackaidja <zackaidja@gmail.com>
2023-11-28 23:11:47 -0500

    another comment out

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,6 @@
 # profiles/urls.py
 from django.urls import path
-from .views import view_profile, edit_profile
+from .views import view_profile  # ,edit_profile
 
 urlpatterns = [
     path("profile/<int:userprofile_id>", view_profile, name="view_profile"),
