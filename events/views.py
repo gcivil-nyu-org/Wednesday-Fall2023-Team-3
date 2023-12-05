d0df4009004dfed2db2cc4242b332cd584252320
Dhiren Motwani <dm5756@nyu.edu>
2023-12-05 14:52:18 -0500

    remove unnecessary changes

--- events/views.py
+++ events/views.py
@@ -559,13 +559,11 @@ def eventDetail(request, event_id):
     user_reaction_emoji = None
     is_favorite = False
     # attempt to see if the user has logged in
-    print(request.user.is_authenticated)
     if request.user.is_authenticated:
         try:
             is_favorite = FavoriteLocation.objects.filter(
                 user=request.user, location=location
             ).exists()
-            print(is_favorite)
             user_reaction = Reaction.objects.get(
                 user=request.user, event=event, is_active=True
             )
